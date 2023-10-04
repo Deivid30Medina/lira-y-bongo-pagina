cb5b86d9e8b9e69474c92397052133d55247cab7
Deivid30Medina <144353360+Deivid30Medina@users.noreply.github.com>
2023-10-04 15:45:50 -0500

    Adelantando mapa sitio

--- js/creandoLirayBongo.js
+++ js/creandoLirayBongo.js
@@ -1,56 +1,407 @@
 //Departamentos al darle un clic.	
-const open1 = document.getElementById('OPEN12');
+const open1 = document.getElementById('OPEN0');
+const open2 = document.getElementById('OPEN1');
+const open3 = document.getElementById('OPEN2');
+const open4 = document.getElementById('OPEN3');
+const open5 = document.getElementById('OPEN4');
+const open6 = document.getElementById('OPEN5');
+const open7 = document.getElementById('OPEN6');
+const open8 = document.getElementById('OPEN7');
+const open9 = document.getElementById('OPEN8');
+const open10 = document.getElementById('OPEN9');
+const open11 = document.getElementById('OPEN10');
+const open12 = document.getElementById('OPEN11');
+const open13 = document.getElementById('OPEN12');
+const open14 = document.getElementById('OPEN13');
+const open15 = document.getElementById('OPEN14');
+const open16 = document.getElementById('OPEN15');
+const open17 = document.getElementById('OPEN16');
+const open18 = document.getElementById('OPEN17');
+const open19 = document.getElementById('OPEN18');
+const open20 = document.getElementById('OPEN19');
+const open21 = document.getElementById('OPEN20');
+const open22 = document.getElementById('OPEN21');
+const open23 = document.getElementById('OPEN22');
+const open24 = document.getElementById('OPEN23');
+const open25 = document.getElementById('OPEN24');
+const open26 = document.getElementById('OPEN25');
+const open27 = document.getElementById('OPEN26');
+const open28 = document.getElementById('OPEN27');
+const open29 = document.getElementById('OPEN28');
+const open30 = document.getElementById('OPEN29');
+const open31 = document.getElementById('OPEN30');
+const open32 = document.getElementById('OPEN31');
+
 
 //Boton de vovler en el popup
-const volver = document.getElementById('idPopupVolver');
+const volver1 = document.getElementById('idPopupVolverChoco');
+const volver2 = document.getElementById('idPopupVolverVichada');
+const volver3 = document.getElementById('idPopupVolverCaqueta');
+const volver4 = document.getElementById('idPopupVolverArauca');
+const volver5 = document.getElementById('idPopupVolverCasanare');
+const volver6 = document.getElementById('idPopupVolverGuainia');
+const volver7 = document.getElementById('idPopupVolverGuaviare');
+const volver8 = document.getElementById('idPopupVolverVaupes');
+const volver9 = document.getElementById('idPopupVolverPutumayo');
+const volver10 = document.getElementById('idPopupVolverAmazonas');
+const volver11 = document.getElementById('idPopupVolverAntioquia');
+const volver12 = document.getElementById('idPopupVolverValledelCauca');
+const volver13 = document.getElementById('idPopupVolverBogota');
+const volver14 = document.getElementById('idPopupVolverAtlantico');
+const volver15 = document.getElementById('idPopupVolverBolivar');
+const volver16 = document.getElementById('idPopupVolverSantander');
+const volver17 = document.getElementById('idPopupVolverCundinamarca');
+const volver18 = document.getElementById('idPopupVolverMagdalena');
+const volver19 = document.getElementById('idPopupVolverCesar');
+const volver20 = document.getElementById('idPopupVolverCordoba');
+const volver21 = document.getElementById('idPopupVolverNorteDeSantander');
+const volver22 = document.getElementById('idPopupVolverBoyaca');
+const volver23 = document.getElementById('idPopupVolverTolima');
+const volver24 = document.getElementById('idPopupVolverCauca');
+const volver25 = document.getElementById('idPopupVolverNarino');
+const volver26 = document.getElementById('idPopupVolverHuila');
+const volver27 = document.getElementById('idPopupVolverGuajira');
+const volver28 = document.getElementById('idPopupVolverSucre');
+const volver29 = document.getElementById('idPopupVolverRisaralda');
+const volver30 = document.getElementById('idPopupVolverQuindio');
+const volver31 = document.getElementById('idPopupVolverCaldas');
+const volver32 = document.getElementById('idPopupVolverMeta');
+
+
+
+
 
 
 //Venta PopUp que se mostrara.
-    const popup1 = document.getElementById('idPopupBogota');
-	const popup2 = document.getElementById('Cesar');
-	const popup3 = document.getElementById('NortSant');
-	const popup4 = document.getElementById('Risaralda');
-	const popup5 = document.getElementById('Vichada');
-	const popup6 = document.getElementById('Caqueta');
-	const popup7 = document.getElementById('Arauca');
-	const popup8 = document.getElementById('Casanare');
-	const popup9 = document.getElementById('Guainia');
-	const popup10 = document.getElementById('Guaviare');
-	const popup11 = document.getElementById('Vaupes');
-	const popup12 = document.getElementById('Putumayo');
-	const popup13 = document.getElementById('Amazonas');
-	const popup14 = document.getElementById('Antioquia');
-	const popup15 = document.getElementById('Atlantico');
-	const popup16 = document.getElementById('Bolivar');
-	const popup17 = document.getElementById('Santander');
-	const popup18 = document.getElementById('Cundinamarca');
-	const popup19 = document.getElementById('Magdalena');
-	const popup20 = document.getElementById('Cordoba');
-	const popup21 = document.getElementById('Boyaca');
-	const popup22 = document.getElementById('Tolima');
-	const popup23 = document.getElementById('Cauca');
-	const popup24 = document.getElementById('Nariño');
-	const popup25 = document.getElementById('Huila');
-	const popup26 = document.getElementById('Guajira');
-	const popup27 = document.getElementById('Sucre');
-	const popup28 = document.getElementById('Quindio');
-	const popup29 = document.getElementById('Caldas');
-	const popup30 = document.getElementById('Meta');
-	const popup31 = document.getElementById('ValleCauca');
-	const popup32 = document.getElementById('SanAndres');
+    const popup1 = document.getElementById('idPopupChoco');
+	const popup2 = document.getElementById('idPopupVichada');
+	const popup3 = document.getElementById('idPopupCaqueta');
+	const popup4 = document.getElementById('idPopupArauca');
+	const popup5 = document.getElementById('idPopupCasanare');
+	const popup6 = document.getElementById('idPopupGuainia');
+	const popup7 = document.getElementById('idPopupGuaviare');
+	const popup8 = document.getElementById('idPopupVaupes');
+	const popup9 = document.getElementById('idPopupPutumayo');
+	const popup10 = document.getElementById('idPopupAmazonas');
+	const popup11 = document.getElementById('idPopupAntioquia');
+	const popup12 = document.getElementById('idPopupValledelCauca');
+	const popup13 = document.getElementById('idPopupBogota');
+	const popup14 = document.getElementById('idPopupAtlantico');
+	const popup15 = document.getElementById('idPopupBolivar');
+	const popup16 = document.getElementById('idPopupSantander');
+	const popup17 = document.getElementById('idPopupCundinamarca');
+	const popup18 = document.getElementById('idPopupMagdalena');
+	const popup19 = document.getElementById('idPopupCesar');
+	const popup20 = document.getElementById('idPopupCordoba');
+	const popup21 = document.getElementById('idPopupNorteDeSantander');
+	const popup22 = document.getElementById('idPopupBoyaca');
+	const popup23 = document.getElementById('idPopupTolima');
+	const popup24 = document.getElementById('idPopupCauca');
+	const popup25 = document.getElementById('idPopupNarino');
+	const popup26 = document.getElementById('idPopupHuila');
+	const popup27 = document.getElementById('idPopupGuajira');
+	const popup28 = document.getElementById('idPopupSucre');
+	const popup29 = document.getElementById('idPopupRisaralda');
+	const popup30 = document.getElementById('idPopupQuindio');
+	const popup31 = document.getElementById('idPopupCaldas');
+	const popup32 = document.getElementById('idPopupMeta');
+	
 
 //Button Cerrar de la ventana PopUp
 
 
-
-//Evento para mostrar el Popup
+// Evento 1
 open1.addEventListener('click', () => {
     popup1.classList.add('show');
 });
 
-//Evento para Ocultar el Popup
-volver.addEventListener('click', () => {
+volver1.addEventListener('click', () => {
     popup1.classList.remove('show');
 });
+
+// Evento 2
+open2.addEventListener('click', () => {
+    popup2.classList.add('show');
+});
+
+volver2.addEventListener('click', () => {
+    popup2.classList.remove('show');
+});
+
+// Evento 3
+open3.addEventListener('click', () => {
+    popup3.classList.add('show');
+});
+
+volver3.addEventListener('click', () => {
+    popup3.classList.remove('show');
+});
+
+// Evento 4
+open4.addEventListener('click', () => {
+    popup4.classList.add('show');
+});
+
+volver4.addEventListener('click', () => {
+    popup4.classList.remove('show');
+});
+
+// Evento 5
+open5.addEventListener('click', () => {
+    popup5.classList.add('show');
+});
+
+volver5.addEventListener('click', () => {
+    popup5.classList.remove('show');
+});
+
+// Evento 6
+open6.addEventListener('click', () => {
+    popup6.classList.add('show');
+});
+
+volver6.addEventListener('click', () => {
+    popup6.classList.remove('show');
+});
+
+// Evento 7
+open7.addEventListener('click', () => {
+    popup7.classList.add('show');
+});
+
+volver7.addEventListener('click', () => {
+    popup7.classList.remove('show');
+});
+
+// Evento 8
+open8.addEventListener('click', () => {
+    popup8.classList.add('show');
+});
+
+volver8.addEventListener('click', () => {
+    popup8.classList.remove('show');
+});
+
+// Evento 9
+open9.addEventListener('click', () => {
+    popup9.classList.add('show');
+});
+
+volver9.addEventListener('click', () => {
+    popup9.classList.remove('show');
+});
+
+// Evento 10
+open10.addEventListener('click', () => {
+    popup10.classList.add('show');
+});
+
+volver10.addEventListener('click', () => {
+    popup10.classList.remove('show');
+});
+
+// Evento 11
+open11.addEventListener('click', () => {
+    popup11.classList.add('show');
+});
+
+volver11.addEventListener('click', () => {
+    popup11.classList.remove('show');
+});
+
+// Evento 12
+open12.addEventListener('click', () => {
+    popup12.classList.add('show');
+});
+
+volver12.addEventListener('click', () => {
+    popup12.classList.remove('show');
+});
+
+// Evento 13
+open13.addEventListener('click', () => {
+    popup13.classList.add('show');
+});
+
+volver13.addEventListener('click', () => {
+    popup13.classList.remove('show');
+});
+
+// Evento 14
+open14.addEventListener('click', () => {
+    popup14.classList.add('show');
+});
+
+volver14.addEventListener('click', () => {
+    popup14.classList.remove('show');
+});
+
+// Evento 15
+open15.addEventListener('click', () => {
+    popup15.classList.add('show');
+});
+
+volver15.addEventListener('click', () => {
+    popup15.classList.remove('show');
+});
+
+// Evento 16
+open16.addEventListener('click', () => {
+    popup16.classList.add('show');
+});
+
+volver16.addEventListener('click', () => {
+    popup16.classList.remove('show');
+});
+
+// Evento 17
+open17.addEventListener('click', () => {
+    popup17.classList.add('show');
+});
+
+volver17.addEventListener('click', () => {
+    popup17.classList.remove('show');
+});
+
+// Evento 18
+open18.addEventListener('click', () => {
+    popup18.classList.add('show');
+});
+
+volver18.addEventListener('click', () => {
+    popup18.classList.remove('show');
+});
+
+// Evento 19
+open19.addEventListener('click', () => {
+    popup19.classList.add('show');
+});
+
+volver19.addEventListener('click', () => {
+    popup19.classList.remove('show');
+});
+
+// Evento 20
+open20.addEventListener('click', () => {
+    popup20.classList.add('show');
+});
+
+volver20.addEventListener('click', () => {
+    popup20.classList.remove('show');
+});
+
+// Evento 21
+open21.addEventListener('click', () => {
+    popup21.classList.add('show');
+});
+
+volver21.addEventListener('click', () => {
+    popup21.classList.remove('show');
+});
+
+// Evento 22
+open22.addEventListener('click', () => {
+    popup22.classList.add('show');
+});
+
+volver22.addEventListener('click', () => {
+    popup22.classList.remove('show');
+});
+
+// Evento 23
+open23.addEventListener('click', () => {
+    popup23.classList.add('show');
+});
+
+volver23.addEventListener('click', () => {
+    popup23.classList.remove('show');
+});
+
+// Evento 24
+open24.addEventListener('click', () => {
+    popup24.classList.add('show');
+});
+
+volver24.addEventListener('click', () => {
+    popup24.classList.remove('show');
+});
+
+// Evento 25
+open25.addEventListener('click', () => {
+    popup25.classList.add('show');
+});
+
+volver25.addEventListener('click', () => {
+    popup25.classList.remove('show');
+});
+
+// Evento 26
+open26.addEventListener('click', () => {
+    popup26.classList.add('show');
+});
+
+volver26.addEventListener('click', () => {
+    popup26.classList.remove('show');
+});
+
+// Evento 27
+open27.addEventListener('click', () => {
+    popup27.classList.add('show');
+});
+
+volver27.addEventListener('click', () => {
+    popup27.classList.remove('show');
+});
+
+// Evento 28
+open28.addEventListener('click', () => {
+    popup28.classList.add('show');
+});
+
+volver28.addEventListener('click', () => {
+    popup28.classList.remove('show');
+});
+
+// Evento 29
+open29.addEventListener('click', () => {
+    popup29.classList.add('show');
+});
+
+volver29.addEventListener('click', () => {
+    popup29.classList.remove('show');
+});
+
+// Evento 30
+open30.addEventListener('click', () => {
+    popup30.classList.add('show');
+});
+
+volver30.addEventListener('click', () => {
+    popup30.classList.remove('show');
+});
+
+// Evento 31
+open31.addEventListener('click', () => {
+    popup31.classList.add('show');
+});
+
+volver31.addEventListener('click', () => {
+    popup31.classList.remove('show');
+});
+
+// Evento 32
+open32.addEventListener('click', () => {
+    popup32.classList.add('show');
+});
+
+volver32.addEventListener('click', () => {
+    popup32.classList.remove('show');
+});
+
+
+
+
+
+
 
 
